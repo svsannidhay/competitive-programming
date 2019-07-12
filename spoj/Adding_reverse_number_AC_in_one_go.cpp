58dcab423d9df23970a9f48a133fc64cec931f6a
Sannidhay vashal <47394582+svsannidhay@users.noreply.github.com>
2019-07-12 05:50:44 -0700

    Add files via upload

---
+++ spoj/Adding_reverse_number_AC_in_one_go.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int reverseadigit(long long int n){
+	long long int reversedn=0,multiplier=0;
+	long long int temp;
+    temp = n;
+    while(temp>0){
+    	temp = temp/10;
+    	multiplier++;
+    }
+	for(long long int i=pow(10,(multiplier-1));n>0;i=(i/10)){
+		int digit = n%10;
+		n = n/10;
+		reversedn = reversedn + (digit*i);
+		//multiplier = multiplier -1;
+	}
+	return reversedn;
+	
+	
+}
+int main(){
+	long long int n;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		long long int n1,n2,nr1,nr2,n;
+		cin>>n1>>n2;
+		nr1 = reverseadigit(n1);
+		nr2 = reverseadigit(n2);
+		n = nr1+nr2;
+		cout<<reverseadigit(n);
+		cout<<"\n";
+	}
+	
+
+	return 0;
+}
