55bd14444bbcf6ba958c2621ab69b413a42ba439
Sannidhay vashal <47394582+svsannidhay@users.noreply.github.com>
2019-07-25 12:08:38 +0530

    TLE removed using fast I/O

--- c++ stl/memorise_me.cpp
+++ c++ stl/memorise_me.cpp
@@ -36,3 +36,43 @@ int main(){
     
     return 0;
 }
+TLE removed using fast I/O:-
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    vector < int > v;
+    vector<int >::iterator it;
+    for(int i=1;i<=n;i++){
+       int x;
+       cin>>x;
+       v.push_back(x);
+    }
+    sort(v.begin(),v.end());
+   
+    int q;
+    cin>>q;
+    for(int i=1;i<=q;i++){
+        int n;
+        cin>>n;
+        auto it2 = lower_bound(v.begin(),v.end(),n);
+        auto it3 = upper_bound(v.begin(),v.end(),n);
+        if((it3-it2)==0){
+            cout<<"NOT PRESENT";
+            cout<<"\n";
+            continue;
+        }
+        cout<<it3-it2;
+        cout<<"\n";
+    }
+    
+    
+    
+    return 0;
+}
+
