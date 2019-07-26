0450548028e72bfa1ccd00359861d22651291612
Sannidhay vashal <47394582+svsannidhay@users.noreply.github.com>
2019-07-26 21:01:18 +0530

    added generalized loop for sieve

--- c++ stl/creating_sieve_prime_problem.cpp
+++ c++ stl/creating_sieve_prime_problem.cpp
@@ -38,3 +38,43 @@ int main(){
     }
     
 }
+//generalised condition for sieve
+#include<iostream>
+#define  fio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#include<vector>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+int main(){
+    fio;
+    vector< int > sieve;
+    for(int i=2;i<100000;i++){
+        sieve.push_back(i);
+    }
+    for(int i=2;i<=sqrt(100000);i++){
+        for(auto j=sieve.begin();j!=sieve.end();j++){
+            
+            if(((*j)>=(i*i))&&(*j)%i==0){
+               *j = -1;
+            }
+            
+        }
+    }
+    sort(sieve.begin(),sieve.end());
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        long long int x;
+        cin>>x;
+        bool present = binary_search(sieve.begin(),sieve.end(),x);
+        if(present == true){
+            cout<<"True";
+            cout<<"\n";
+        }
+        if(present == false){
+            cout<<"False";
+            cout<<"\n";
+        }
+    }
+    
+}
