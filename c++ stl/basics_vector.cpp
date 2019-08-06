4a66ce18632a962e5b78abd66f8cb82f5ba2b860
Sannidhay vashal <47394582+svsannidhay@users.noreply.github.com>
2019-08-06 20:12:02 +0530

    erase functions added

--- c++ stl/basics_vector.cpp
+++ c++ stl/basics_vector.cpp
@@ -10,16 +10,16 @@ bool f(int x, int y){
 int main(){
     vector<int> A ={3,11,2,14};
     cout<<A[1]<<endl;
-    sort(A.begin(),A.end());   //(NlogN)
-    bool present = binary_search( A.begin() , A.end() , 3 ); //true
-    cout<<present<<endl;
-    present = binary_search( A.begin() , A.end() , 4 );    //false
-    cout<<present<<endl;
+    sort(A.begin(),A.end());   //(NlogN)                              //sorting in the vector
+    bool present = binary_search( A.begin() , A.end() , 3 ); //true   /* binary search function
+    cout<<present<<endl;                                                ..
+    present = binary_search( A.begin() , A.end() , 4 );    //false      binary search function
+    cout<<present<<endl;                                                */
     
     A.push_back(100);                                                                 
     present = binary_search( A.begin() ,A.end() , 100);
     cout<<present<<endl;
-    A.push_back(100);
+    A.push_back(100);                       //push back function
     A.push_back(100);
     A.push_back(100);
     A.push_back(100);
@@ -38,11 +38,14 @@ int main(){
     }
     */
     for(int &x: A){
-        x++;              //reference
+        x++;              //printing using reference
     }
     for(int x: A){
         cout<<x<<" ";      //printing
     }
+    A.erase(v.begin()+4) //erases the fifth element from the vector
+    A.erase(v.begin()+2,v.begin()+4) //erases all the element s from the third element to the fifth  element    
     
-    return 0;
+    
+     return 0;
 }
