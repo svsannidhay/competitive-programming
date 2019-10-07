3eb78f0a4c3e4e02324b61f31388a624d3331ef2
Sannidhay vashal <47394582+svsannidhay@users.noreply.github.com>
2019-10-07 18:47:47 +0530

    Update intelligent_girl_hackerearth.cpp

--- dynamic_programming_practice/intelligent_girl_hackerearth.cpp
+++ dynamic_programming_practice/intelligent_girl_hackerearth.cpp
@@ -39,52 +39,7 @@ int main(){
     }
     if(s[s.length()-1]%2==0){
         cout<<1;
-    }
-    else{
-        cout<<0;
-    }
-}
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#define fio ios_base::sync_with_stdio(false);cin.tie(NULL)
-#define ull unsigned long long int
-#define MOD 1000000007
-using namespace std;
-vector<ull> dp;
-ull index = 0;
-ull create_dp(string s){
-    if(index == s.length()){
-            return 0;
-    }
-    if((s[index])%2==0){
-        //s.pop_back();
-        index++;
-        ull val = 1 + create_dp(s);
-        dp.push_back(val);
-        return val;
-    }
-    else{
-        //s.pop_back();
-        index++;
-        ull val = create_dp(s);
-        dp.push_back(val);
-        return val;
-    }
-    
-}
-int main(){
-    string s,test;
-    fio;    
-    cin>>s;
-    test = s;
-    (create_dp(test));
-    for(auto it = dp.end()-1;it!=dp.begin();it--){
-        cout<<*it<<" ";
-    }
-    if(s[s.length()-1]%2==0){
-        cout<<1;
-    }
+    }                          // lol i am a noob for now thats why i dont know where i am missing the base ,obviously i am not that good with recursions
     else{
         cout<<0;
     }
