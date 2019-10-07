79e0a3f561f75ef254ce629f59905ae9bbc014a7
Sannidhay vashal <47394582+svsannidhay@users.noreply.github.com>
2019-10-07 17:33:53 +0530

    Update Wacky_Workout_hackerearth.cpp

--- dynamic_programming_practice/Wacky_Workout_hackerearth.cpp
+++ dynamic_programming_practice/Wacky_Workout_hackerearth.cpp
@@ -2,33 +2,6 @@
 // easy 
 //However, since N is upto 10^18, you must use matrix exponentiation to find the answer in O(log n) time.
 //dp(n) = dp(n-1) + dp(n-2)
-/*
-// Sample code to perform I/O:
-
-cin >> name;                            // Reading input from STDIN
-cout << "Hi, " << name << ".\n";        // Writing output to STDOUT
-
-// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
-*/
-
-// Write your code here
-/*
-// Sample code to perform I/O:
-
-#include <iostream>
-
-using namespace std;
-
-int main() {
-	int num;
-	cin >> num;										// Reading input from STDIN
-	cout << "Input number is " << num << endl;		// Writing output to STDOUT
-}
-
-// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
-*/
-
-// Write your code here
 #include<iostream>
 #include<algorithm>
 #include<vector>
