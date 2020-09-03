f0ea6e930a6d08d7215140dd7cf89ddfe1345295
Sannidhay vashal <47394582+svsannidhay@users.noreply.github.com>
2020-09-03 18:09:39 +0530

    Create BIlliards.cpp

---
+++ dynamic_programming_practice/practice problems /BIlliards.cpp
@@ -0,0 +1,141 @@
+//https://www.codechef.com/LRNDSA07/problems/CDQU5
+/*
+	Solution by:- 
+	--------------Sannidhay Vashal
+	----------------NIT SRINAGAR
+ 
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<stack>
+#include<queue>
+#include<map>
+#include<math.h>
+#include<climits>
+#include<set>
+#include<unordered_map>
+#include<cstring>
+ 
+#define fio ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define ll            long long int
+#define ull           unsigned long long int
+#define cinll(x)      ll x;cin>>x;
+#define cini(x)       int x;cin>>x;
+#define cins(x)       string x;cin>>x;
+#define vect(x)       vector<ll> x;
+#define vect1(x)      vector<ll> x;x.push_back(0);
+#define pb(x)         push_back(x)
+#define mp(x,y)       make_pair(x,y)
+#define MAX           1e18
+#define MIN           -1e18
+#define MOD           1000000009
+#define vectdef(x,size,val) vector<ll> x;for(ll i=0;i<size;i++){x.push_back(val);}
+#define vectdef1(x,size,val) vector<ll> x;for(ll i=0;i<=size;i++){x.push_back(val);}
+
+
+using std::string;
+
+static struct IO {
+    char tmp[1 << 10];
+    // fast input routines
+    char cur;
+//#define nextChar() (cur = getc_unlocked(stdin))
+//#define peekChar() (cur)
+    inline char nextChar() { return cur = getc_unlocked(stdin); }
+    inline char peekChar() { return cur; }
+    inline operator bool() { return peekChar(); }
+    inline static bool isBlank(char c) { return (c < '-' && c); }
+    inline bool skipBlanks() { while (isBlank(nextChar())); return peekChar() != 0; }
+    inline IO& operator >> (char & c) { c = nextChar(); return *this; }
+    inline IO& operator >> (char * buf) {
+        if (skipBlanks()) {
+            if (peekChar()) {
+                *(buf++) = peekChar();
+                while (!isBlank(nextChar())) *(buf++) = peekChar();
+            } *(buf++) = 0; } return *this; }
+    inline IO& operator >> (string & s) {
+        if (skipBlanks()) { s.clear(); s += peekChar();
+            while (!isBlank(nextChar())) s += peekChar(); }
+        return *this; }
+    inline IO& operator >> (double & d) { if ((*this) >> tmp) sscanf(tmp, "%lf", &d); return *this; }
+#define defineInFor(intType) \
+    inline IO& operator >>(intType & n) { \
+        if (skipBlanks()) { \
+            int sign = +1; \
+            if (peekChar() == '-') { \
+                sign = -1; \
+                n = nextChar() - '0'; \
+            } else \
+                n = peekChar() - '0'; \
+            while (!isBlank(nextChar())) { \
+                n += n + (n << 3) + peekChar() - 48; \
+            } \
+            n *= sign; \
+        } \
+        return *this; \
+    }
+defineInFor(int)
+defineInFor(unsigned int)
+defineInFor(long long)
+    // fast output routines
+//#define putChar(c) putc_unlocked((c), stdout)
+    inline void putChar(char c) { putc_unlocked(c, stdout); }
+    inline IO& operator << (char c) { putChar(c); return *this; }
+    inline IO& operator << (const char * s) { while (*s) putChar(*s++); return *this; }
+    inline IO& operator << (const string & s) { for (int i = 0; i < (int)s.size(); ++i) putChar(s[i]); return *this; }
+    char * toString(double d) { sprintf(tmp, "%lf%c", d, '\0'); return tmp; }
+    inline IO& operator << (double d) { return (*this) << toString(d); }
+#define defineOutFor(intType) \
+    inline char * toString(intType n) { \
+        char * p = (tmp + 30); \
+        if (n) { \
+            bool isNeg = 0; \
+            if (n < 0) isNeg = 1, n = -n; \
+            while (n) \
+                *--p = (n % 10) + '0', n /= 10; \
+            if (isNeg) *--p = '-'; \
+        } else *--p = '0'; \
+        return p; \
+    } \
+    inline IO& operator << (intType n) { return (*this) << toString(n); }
+defineOutFor(int)
+defineOutFor(long long)
+#define endl ('\n')
+#define cout _io_
+#define cin _io_
+} _io_;
+using namespace std;
+
+//top-down memoized soltion 
+//time complexity O(n)
+ll memo[1000005];
+ll solvememo(ll n){
+    if(n==2) return 1;
+    if(n==3) return 1;
+    if(n<2) return 0;
+    if(memo[n]!=-1) return memo[n];
+    memo[n] = (solvememo(n-2) + solvememo(n-3))%MOD;
+    return memo[n];
+}
+//bottom - up pure dp
+ll dp[1000005];
+void solvedp(){
+    memset(dp,0,sizeof(dp));
+    dp[2] = 1;dp[3] = 1;
+    for(ll i=4;i<=1000005;i++){
+        dp[i] = (dp[i-2] + dp[i-3])%MOD;
+    }
+}
+
+int main(){
+    cinll(t);
+    //memset(memo,-1,sizeof(memo));
+    solvedp();
+    for(ll i=0;i<t;i++){
+        cinll(n);
+        cout<<dp[n]<<"\n";
+        //cout<<solvememo(n)<<"\n";
+    }    
+    return 0;
+}
